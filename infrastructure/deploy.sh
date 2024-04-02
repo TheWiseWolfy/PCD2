ca137d1ad90f6e3a2096f716d621d8fb40f92337
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-04-02 22:20:46 +0300

    Homework 2: Resize infra

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-sam build -t compute.yaml
 sam deploy --profile pcd-homework-2 -t db.yaml --stack-name pcd-homework-2-db
 sam deploy --profile pcd-homework-2 -t cache.yaml --stack-name pcd-homework-2-cache
-sam deploy --profile pcd-homework-2 -t compute.yaml --stack-name pcd-homework-2-compute
+
+sam build -t compute.yaml
+sam package --profile pcd-homework-2 --template-file .aws-sam/build/template.yaml --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-3ssirkjypfff --output-template-file packaged-template.yaml
+sam deploy --profile pcd-homework-2 -t packaged-template.yaml --stack-name pcd-homework-2-compute
