23724d4acf718d198dab22bab80f6f4e76b1fe65
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-04-07 04:12:25 +0300

    Homework 2: Refactor add more visualisations

--- frontend/src/components/visualisations/addMoreVisualisations.tsx
+++ frontend/src/components/visualisations/addMoreVisualisations.tsx
@@ -2,6 +2,9 @@ import { FC } from "react";
 import { Card } from "../card/card";
 import { Button } from "../input/button";
 import { H3 } from "../typography/h3";
+import { Image } from "../image/image";
+import { Spacing } from "../spacing/spacing";
+import { CardSpaceElement } from "../card/cardSpaceElement";
 
 interface Props {
     onGoToCreateVisualisation(): void
@@ -9,8 +12,12 @@ interface Props {
 
 export const AddMoreVisualisations: FC<Props> = ({ onGoToCreateVisualisation }) => {
     return (
-        <Card width={384} centered>
+        <Card width={384} centered noShadow>
             <H3>Need more?</H3>
+            <CardSpaceElement />
+            <Image id="chart" size="xl" scaleFactor={5} />
+            <Spacing spacing="m" />
+            <CardSpaceElement />
             <Button onClick={onGoToCreateVisualisation}>Add more</Button>
         </Card>
     )
